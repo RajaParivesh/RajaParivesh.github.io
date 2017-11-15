e1d3134be338a3501d10a3565f7c383cf5f36d01
RajaParivesh <rajaparivesh@gmail.com>
2017-11-15 11:11:45 +0530

    #1 Website uploaded

---
+++ main.js
@@ -0,0 +1,8 @@
+document.write('This is a javascript file');
+
+var registrationButton = document.querySelctorAll('.registration');
+
+registrationButton.onclick = function(){
+	alert('Are you sure want to register?');
+}
+
